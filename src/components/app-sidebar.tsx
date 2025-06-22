1fe53dc687f0e0a5932f4a5ef33e1443f0171a72
Flappy <stvnsbarrantes@gmail.com>
2025-06-22 18:48:47 +0800

    styles: Cardm carousel, sidebar, Modal

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import type * as React from "react"
-import { GalleryVerticalEnd } from "lucide-react"
+import { PillBottle } from "lucide-react"
 
 import {
   Sidebar,
@@ -9,9 +9,6 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
   SidebarRail,
 } from "@/components/ui/sidebar"
 
@@ -19,35 +16,18 @@ import {
 const data = {
   navMain: [
     {
-      title: "Products",
-      url: "#",
-      items: [
-        {
-          title: "List",
-          url: "#",
-        },
-        {
-          title: "Archive",
-          url: "#",
-        },
-       
-      ],
+      title: "Dashboard",
+      url: "/dashboard",
+      
+    },
+    {
+      title: "Medicine",
+      url: "/medicine",
     },
     {
       title: "Inventory",
-      url: "#",
-      items: [
-        {
-          title: "List",
-          url: "#",
-        },
-        {
-          title: "Archive",
-          url: "#",
-        },
-       
-      ],
-    },
+      url: "/inventory",
+    }
   ],
 }
 
@@ -60,11 +40,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
+                  <PillBottle className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
+                  <span className="font-semibold">Medicine Inventory</span>
                 </div>
               </a>
             </SidebarMenuButton>
@@ -81,17 +60,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     {item.title}
                   </a>
                 </SidebarMenuButton>
-                {item.items?.length ? (
-                  <SidebarMenuSub>
-                    {item.items.map((item) => (
-                      <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
-                          <a href={item.url}>{item.title}</a>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                ) : null}
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
