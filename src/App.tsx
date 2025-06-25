2fb00b23eba90e46f9c1ec2497c8d636cd75a9fe
Flappy <stvnsbarrantes@gmail.com>
2025-06-25 16:18:32 +0800

    update:

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 import Login from './user/login'
 import Medicine from './medicine/medicine'
@@ -6,13 +6,18 @@ import MedicineCreate from '@/medicine/CUD/create'
 import MedicineUpdate from '@/medicine/CUD/update'
 import DashBoard from '@/dashboard/index'
 import InventoryView from '@/inventory/viewInventory'
-
+import { usePersistUser } from '@/api/user/Api_user'
 function App() {
+  const isAuth = usePersistUser((state) => state.isAuth);
+ console.log("isAuth", isAuth);
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Login/>} />
+          <Route path="/" element={
+            isAuth ? <Navigate to="/dashboard" replace /> : 
+          <Login />
+          } />
           <Route path="/medicine" element={<Medicine/>} />
           <Route path='/medicine/create' element={<MedicineCreate/>} />
           <Route path='/medicine/update/:id' element={<MedicineUpdate/>} />
